ed943f172c108b270efcd71a0a0ea7209fb3390f
Richard Warburton <richard.warburton@gmail.com>
2016-11-30 17:00:50 +0000

    print date out at the end

--- countdown.sh
+++ countdown.sh
@@ -2,11 +2,14 @@
 
 set -eu
 
+date
+
 time=$(($1*60))
 date1=$((`date +%s` + time)); 
 while [ "$date1" -ge `date +%s` ]; do 
   echo -ne "$(date -u --date @$(($date1 - `date +%s`)) +%H:%M:%S)\r";
   sleep 0.1
 done
+date
 mplayer train_door_beep.wav
 
