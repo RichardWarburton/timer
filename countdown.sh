601d6cdaaa28ae4a2ecac929270a54fb57fa4f9d
Richard Warburton <richard.warburton@gmail.com>
2017-03-21 23:06:31 +0200

    consistent use of -u

--- countdown.sh
+++ countdown.sh
@@ -5,9 +5,9 @@ set -eu
 date
 
 time=$(($1*60))
-date1=$((`date +%s` + time)); 
-while [ "$date1" -ge `date +%s` ]; do 
-  echo -ne "$(date -u --date @$(($date1 - `date +%s`)) +%H:%M:%S)\r";
+date1=$((`date -u +%s` + time)); 
+while [ "$date1" -ge `date -u +%s` ]; do 
+  echo -ne "$(date -u --date @$(($date1 - `date -u +%s`)) +%H:%M:%S)\r";
   sleep 0.1
 done
 date
